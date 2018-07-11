bc551dbc66964ac0ee279c8d561edb3a86fb392a
Bruno CESSAC <bcessac@hotmail.com>
2018-07-11 22:45:46 +0200

    implemant READ

---
+++ app/api/tweets.py
@@ -0,0 +1,19 @@
+# app/api/tweets.py
+from flask import Blueprint, jsonify
+from app.db import tweet_repository
+# TODO: import tweet_repository
+
+api = Blueprint('tweets', __name__)
+
+
+@api.route('/tweets/<int:id>')
+def show(id):
+    tweet = tweet_repository.get(id)
+    return jsonify({
+        "id": tweet.id,
+        "text": tweet.text,
+        "created_at": tweet.created_at
+    })
+
+
+# TODO: implement the `/tweets/1` parametric route
--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ from datetime import datetime
 class Tweet:
     def __init__(self, message):
         self.text = message
-        self.created_at = datetime
+        self.created_at = datetime.now()
         self.id = None
